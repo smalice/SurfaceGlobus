9bf74518de0fd9e0a2a683b4756671432696f489
felo <felipe.longe@capgemini.com>
2011-04-06 15:36:15 +0200

    added popup control

---
+++ InfoStratSample/InfoStratSample/ZoomItem.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Surface;
+using Microsoft.Surface.Presentation;
+using Microsoft.Surface.Presentation.Controls;
+
+namespace InfoStratSample
+{
+    /// <summary>
+    /// Interaction logic for ZoomItem.xaml
+    /// </summary>
+    public partial class ZoomItem : SurfaceUserControl
+    {
+        public ZoomItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
